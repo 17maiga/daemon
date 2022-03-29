a5fe96390abe1f84d3932d6c4a85da93a0686ad7
maiga <henri.saudubray@gmail.com>
2022-03-29 20:04:55 +0200

    First communication between demon and invoker

--- demon.c
+++ demon.c
@@ -2,8 +2,30 @@
 // Created by maiga on 23/03/2022.
 //
 #include <stdio.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
 
 int main() {
-    printf("demon invoked");
+    int status = 1;
 
+    time_t invoked = time(NULL);
+
+    char* incantation_path = "/tmp/daemon.incantation.txt", incantation[50];
+
+    int incantation_file = open(incantation_path, O_RDONLY);
+    FILE* incantation_stream = fdopen(incantation_file, "r");
+    fscanf(incantation_stream, "%s", incantation);
+    printf("%s", incantation);
+//    puts(incantation);
+//    unlink(incantation_path);
+//    return 0;
+    /*
+    char* curse_path = "/tmp/daemon.curse.txt";
+    int curse;
+    FILE* curse_stream;
+    while (status) {
+
+    }
+    */
 }
--- demon.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by maiga on 23/03/2022.
-//
-
-#ifndef DAEMON_DEMON_H
-#define DAEMON_DEMON_H
-
-#endif //DAEMON_DEMON_H
